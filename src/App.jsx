9b5dab682ef8e11fb0f244a6d6c35cb93053c16d
Tim <50870607+toothless13@users.noreply.github.com>
2023-12-06 15:56:58 +0000

    create basic components, boilerplate and add to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-// import { useState } from 'react'
+import NavBar from "./components/NavBar"
+import Intro from "./components/Intro"
+import Projects from "./components/Projects"
+import Skills from "./components/Skills"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 
 function App() {
 
   return (
-    <>
-    Portfolio
-    </>
+    <div className="App">
+      <NavBar />
+      <Intro />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </div>
   )
 }
 
