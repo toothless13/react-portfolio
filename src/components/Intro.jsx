81b9d59994077b89ac4c2343eb24ef7cafe0917b
Tim <50870607+toothless13@users.noreply.github.com>
2023-12-08 15:53:36 +0000

    start navbar and intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,20 @@
 const Intro = () => {
   return (
-    <div>Intro</div>
+    <div className="flex items-center justify-center flex-col text-center pt-2 pb-4">
+      <h1 className="text-4xl md:text-6xl mb-2 md:mb-3 mt-2 md:mt-3 font-bold">timCodes</h1>
+      <p className="text-base md:text-xl mb-4 md:mb-6 font-medium">Software Engineer & Web Developer</p>
+      <p className="text-sm max-w-xl mb-6 text-left">
+        Hi, my name is Tim, and I&apos;m a Junior Software Developer.
+        <br /><br/>
+        I&apos;ve been learning and creating projects for just over a year, and created this website to show my recent projects, and the skills that I&apos;ve acquired.
+        <br /><br />
+        I&apos;m currently most familiar with HTML, CSS, JavaScript and Node, and my next goal is to become familiar with and learn about React.
+        <br /><br />
+        This page will be updated as my range of skills increases and as and when I complete new projects.
+        <br /><br />
+        As well as coding, I also enjoy playing guitar, video games and watching Formula 1 & anime! I&apos;m also currently learning Japanese in my spare time.
+      </p>
+    </div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,17 @@
+import { v4 as uuidv4 } from "uuid";
+
 const NavBar = () => {
+
+  const navItems = ["Logo", "Projects", "Skills", "Contact"];
+
   return (
-    <div>NavBar</div>
+    <div className="flex items-center justify-center text-center pt-5 pb-6">
+      <nav className="flex justify-between w-1/4 mx-auto">
+        {navItems.map(navItem => {
+          return <a href="#" key={uuidv4()} className="px-5">{navItem}</a>
+        })}
+      </nav>
+    </div>
   )
 }
 
