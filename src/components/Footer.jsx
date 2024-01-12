5d87febe4951f63c3b4bf86b37c17b3facd7d3a8
Tim <50870607+toothless13@users.noreply.github.com>
2024-01-12 12:54:39 +0000

    add initial footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,11 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="py-5 text-center">
+      {/* {Social Icons} */}
+      <p className="text-sm mt-2 opacity-50">
+        &copy; {new Date().getFullYear()} timCodes. All rights reserved.
+      </p>
+    </div>
   )
 }
 
